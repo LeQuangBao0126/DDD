a2681a3068fc1d025964809c44248325894d42d7
baonasht <nhoctengi6789@gmail.com>
2021-08-07 15:53:20 +0700

    Create Entity in Domain

---
+++ Exam/Exam.Domain/AggregateModels/CategoryAggregate/Category.cs
@@ -0,0 +1,23 @@
+﻿using Exam.Domain.SeedWork;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exam.Domain.AggregateModels.CategoryAggregate
+{
+    public class Category : Entity , IAggregateRoot
+    {
+        [BsonElement("name")]
+        public string  Name { get; set; }
+
+        [BsonElement("urlPath")]
+        public string  UrlPath { get; set; }  //domain/exam-category-1
+
+
+
+
+    }
+}
